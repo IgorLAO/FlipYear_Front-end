c4b869564a2612e9c54c5ff8fa09711693769032
d3nz-17 <denzelnatha30@gmail.com>
2023-09-06 16:49:22 -0300

    catalogo dos filtros (faltas os cards)

--- src/pages/catalogo/index.jsx
+++ src/pages/catalogo/index.jsx
@@ -1,13 +1,17 @@
 import './index.scss';
 import filtro from '../../ui/images/filtro_logo.png'
+import Rodape from '../../ui/components/rodape/index.js'
+
+import Cabecalho from '../../ui/components/cabecalho/cabecalho.js'
 export default function Catalogo(){
     return(
         <>
             <div className="container-ctlg">
-                   <h1>Catálogo</h1> 
+                <Cabecalho></Cabecalho>
+                   <h1 className='ctlg'>Catálogo</h1> 
                 <div className="bloco-filtro-ctlg">
                     <div>
-                        <img src={filtro}/>
+                        <img src={filtro} className='filtro'/>
                         <p>Tipo de Produto</p>
 
                     </div>
@@ -24,7 +28,20 @@ export default function Catalogo(){
                         </div>
                     </div>
                 </div>
-                <h1>Exibindo todos os resultados para {}</h1>
+                <h1 className='exib'>Exibindo todos os resultados para {}</h1>
+
+                <div className='paginacao'>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+                <p>7</p>
+                <p> seta </p>
+                </div>
+                <Rodape></Rodape>
+
 
             </div>
         
