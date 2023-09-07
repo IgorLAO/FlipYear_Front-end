af1ea6954697651cafccfc1bc68a3dec121bb3de
main <italoalcantara6@gmail.com>
2023-09-07 20:32:24 -0300

    rodape

--- src/ui/components/rodape/index.js
+++ src/ui/components/rodape/index.js
@@ -14,132 +14,82 @@ import Instagram from '../../images/instagram_logo.png'
 import Facebook from '../../images/facebook_logo.png'
 
 
-
-
-
-
-
 export default function Rodape(){
-
-
     return(
+        <>
+            <div className="container_rodape">
+                <div className="topicos">
+                    <div >
+                        <h1>CONTEÚDO</h1>
 
-        <div className='comp-rodape'>
+                        <a>Fale conosco</a>
+                        <a>Minha Conta</a>
+                        <a>A Empresa</a>
+                        <a>Atendimento ao Cliente</a>
+                        <a>Garantia e Devoluções</a>
+                        <a>Política de Privacidade</a>
+                    </div>
+                    <div>
+                        <h1>CATEGORIAS</h1>
 
+                        <a>Nintendo</a>
+                        <a>Nintendo</a>
+                        <a>Nintendo</a>
+                        <a>Nintendo</a>
+                        <a>Nintendo</a>
+                        <a>Nintendo</a>
+                    </div>
+                    <div className="contato">
+                        <h1>CONTATO</h1>
 
-        <div className='infos'>
+                        <div>
+                            <img src={Tel}/>
+                            <p>Telefone:(11)9999-9999</p>
+                        </div>
+                        <div>
+                            <img src={ZAP}/>
+                            <p>Whatsapp: (11)9999-9999</p>
+                        </div>
+                        <div>
+                            <img src={Email}/>
+                            <p>E-mail: garotodeprograma@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="formas_pagamento">
+                        <h1>FORMAS DE PAGAMENTO</h1>
+                        <div className="images_pagamento">
+                            <div>
+                                <img src={Visa} />  
+                                <img src={MercadoPago}/>
+                                <img src={MasterCard}/>
+                            </div>
+                            <div>
+                                <img src={HyperCard}/>
+                                <img src={Pix}/>
+                                <img src={Boleto}/>
+                            </div>
+                        </div>
+                        <div className="redes_sociais">
+                            <h1>Siga-nos nas Redes Sociais</h1>
 
-        <div className='conteudo'>
+                            <div>
+                                <img src={Facebook}/>
+                                <img src={Instagram}/>
+                                <img src={Youtube}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="cert_seguranca">
+                        <h1>CERTIFICADOS DE SEGURANÇA</h1>
 
-        <h3>Conteúdo</h3>
-
-        <p>Fale Conosco</p>
-        <p>Minha Conta</p>
-        <p>A Empresa</p>
-        <p>Atendimento ao Cliente</p>
-        <p>Garantias e Devoluções</p>
-        <p>Política de Privacidade</p>
-
-        <div className='certificados'>
-
-<h3>certificados de segurança</h3>
-
-<img src={CertSeg}/>
-
-</div>
-
-
-        </div>
-
-        <div className='categorias'>
-
-        <h3>Categorias</h3>
-
-        <p>Consoles</p>
-        <p>Jogos</p>
-        <p>Colecionador</p>
-
-
-        </div>
-
-        <div className='contato'>
-
-        <h3>Contato</h3>
-
-        <div className='contato-linha'>
-        <img src={Tel}/><p>Telefone 
-        <br/> (11) 9999-9999
-        </p>
-        </div>
-
-
-        <div className='contato-linha'>
-        <img src={ZAP}/><p>Whatsapp: 
-        <br/> (11) 9999-9999
-        </p>
-        </div>
-
-
-        <div className='contato-linha'> 
-        <img src={Email}/><p>E-mail: 
-        <br/> manoelgomes@gmail.com
-        </p>
-        </div>
-        </div>
-
-    <div className='info-2'>
-
-    <div className='formas-pag'>
-            <h3>Formas de pagamento</h3>
-
-            <div className='imagens-pag'>
-            <img src={Visa}/>
-            <img src={MercadoPago}/>
-            <img src={MasterCard}/>
+                        <img src={CertSeg}/>
+                </div>
+                    <div className="copyright">
+                        <div></div>
+                        <p>Copyright © 2023 FLIPER2000 Inc. Todos os direitos reservados.</p>
+                    </div>
             </div>
-
-            <div className='imagens-pag'>
-            <img src={HyperCard}/>
-            <img src={Pix}/>
-            <img src={Boleto}/>
-            </div>
-
-
-        <div className='redes-sociais'>
-
-            <h6>Siga-nos nas redes sociais</h6>
-
-            <img src={Facebook}/>
-            <img src={Instagram}/>
-            <img src={Youtube}/>
-        </div>
-
-    </div>
-
-        
-        </div>
-
-        </div>
-
-        
-
-
-
-        <hr/>
-
-        
-
-        <p>Copyright © 2023 FLIPER2000 Inc. Todos os direitos reservados.</p>
-
-
-
-        </div>
-
-
-
+        </>
     )
-
-
-
-
 }
