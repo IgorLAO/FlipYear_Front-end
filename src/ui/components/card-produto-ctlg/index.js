cbf48a5f474421ba34808d85c22806e43fca1e3e
d3nz-17 <denzelnatha30@gmail.com>
2023-09-07 16:51:54 -0300

    cards (99%, da pra usar ja)

--- src/ui/components/card-produto-ctlg/index.js
+++ src/ui/components/card-produto-ctlg/index.js
@@ -11,21 +11,27 @@ export default function CardProdutoCtlg(){
 
                 <img src={Atari}></img>
                 <p className='nm-produto'> Atari 3000 </p>
-                <p className='info-produto'> Atari inc. <p>•</p> 
-                <p className='estado-produto'>Seminovo</p>
-                </p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
+
+                <div className='desc-info'>
+                <p className='info-produto' id='cinza-pequeno'> Atari inc. </p>
+                <p id='cinza-pequeno' className='ponto'>•</p> 
+                <p className='estado-produto' id='cinza-pequeno'>Seminovo</p>
+                
+                </div>
+                <svg className = 'linha' xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
                 <path d="M0.816406 1.22266H282.724" stroke="#0D3A3D" stroke-linecap="round"/>
                 </svg>
                 <p className='preco-produto'>R$ 99,90 </p>
-                <p className='prec-produto-original'>RS$ 999,90 </p>
+                <p className='prec-produto-original' id='cinza-pequeno'>RS$ 999,90 </p>
 
                 <div className='avaliacao-produto'>
+                    <div className='estrelas'>
                     <img src={Estrela}></img>
                     <img src={Estrela}></img>
                     <img src={Estrela}></img>
                     <img src={Estrela}></img>
                     <img src={Estrela}></img>
+                    </div>
 
                     <p className='avaliacao-decimal'>(3,5)</p>
 
