c761242f229e1d2a819032e71d42d55cd3c77e2d
igorLAO <igooli670@gmai.com>
2023-09-07 21:48:53 -0300

    tentei organizar os assets

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -4,9 +4,7 @@ import Rodape from '../../ui/components/rodape';
 function App() {
   return (
     <div className="App">
-      <h1>
-        FLIP YEAR
-      </h1>
+      <h1>  </h1>
 
       <Rodape></Rodape>
     </div>
