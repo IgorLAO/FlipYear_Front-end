d647f0d40dd1855bdc75466aff8f7dd3ddfb0931
igorLAO <igooli670@gmai.com>
2023-09-09 18:15:43 -0300

    consulta usuarios completa

--- src/pages/ADM_pages/consulta_usuarios/index.js
+++ src/pages/ADM_pages/consulta_usuarios/index.js
@@ -19,6 +19,14 @@ export default function Users_Consulta() {
                 </span>
 
                 <table>
+                <colgroup>
+                                <col style={{ width: 1 + '%' }} />
+                                <col style={{ width: 15 + '%' }} />
+                                <col style={{ width: 5 + '%' }} />
+                                <col style={{ width: 10 + '%' }} />
+                                <col style={{ width: 1 + '%' }} />
+                                <col style={{ width: 1 + '%' }} />
+                            </colgroup>
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -35,7 +43,23 @@ export default function Users_Consulta() {
                             <td> carlinhos </td>
                             <td> 54654654654 </td>
                             <td> carlinhos@gmail.com </td>
-                            <td> 4 </td>
+                            <td> 69 </td>
+                            <td> <button> ... </button> </td>
+                        </tr>
+                        <tr>
+                            <td> 1 </td>
+                            <td> carlinhos </td>
+                            <td> 54654654654 </td>
+                            <td> carlinhos@gmail.com </td>
+                            <td> 69 </td>
+                            <td> <button> ... </button> </td>
+                        </tr>
+                        <tr>
+                            <td> 1 </td>
+                            <td> carlinhos </td>
+                            <td> 54654654654 </td>
+                            <td> carlinhos@gmail.com </td>
+                            <td> 666 </td>
                             <td> <button> ... </button> </td>
                         </tr>
                     </tbody>
