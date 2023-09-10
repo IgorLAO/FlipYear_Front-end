9c056b48f14ada7d73654ec1d89ce2af7d280dab
d3nz-17 <denzelnatha30@gmail.com>
2023-09-10 14:50:20 -0300

    perfil pessoal cm os pedidos (feito)

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import PerfilPublico from './pages/perfil/perfil-publico'
 import Pagamento75 from './pages/pagamento75';
 import AdmHome from './pages/ADM_pages/home';
 import Users_Consulta from './pages/ADM_pages/consulta_usuarios';
+import PerfilPessoal from './pages/perfil/perfil-pessoal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,6 +27,7 @@ root.render(
         <Route path='/pagamento50' element={<Pagamento50 />} />
         <Route path='/pagamento75' element={<Pagamento75 />} />
         <Route path='/perfil-publico' element={<PerfilPublico />} />
+        <Route path='/perfil-pessoal' element={<PerfilPessoal />} />
         <Route path='/ADM' element={<AdmHome />} />
         <Route path='/ADM_Users' element={<Users_Consulta />} />
         
