2cc420783a60c72edab2e962886bfc3316d3e6e7
DavidMartins18 <familymartins10mil@gmail.com>
2023-09-12 09:30:06 -0300

    pastas e home

--- src/pages/esqueceusenha/index.jsx
+++ src/pages/esqueceusenha/index.jsx
@@ -3,22 +3,20 @@ import './index.scss';
 export default function Esqueceu(){
 
     return (
-<>
-</>
+        <div className='main'>
+            <h1> Recuperar senha </h1>
 
+            <div className='prt-1'>
+                <p className='bloco-1'> Email </p>
+                <input type="text" />
+            </div>
+            <div className='prt-2'>
+                <p className='bloco-2'> Confirme Email </p>
+                <input type="text" />
+            </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
+            <button>Enviar</button>
+        </div>
     );
 
 
