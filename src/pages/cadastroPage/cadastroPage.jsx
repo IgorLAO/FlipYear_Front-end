fa4058032a9ca723b68973eae09b868f4ca12a73
DavidMartins18 <141844271+DavidMartins18@users.noreply.github.com>
2023-09-14 14:20:17 -0300

    pasta e home

--- src/pages/cadastroPage/cadastroPage.jsx
+++ src/pages/cadastroPage/cadastroPage.jsx
@@ -14,24 +14,24 @@ const Cadastro = () => {
             <h1>Cadastro</h1>
             <p className='prt-0'> Informe os seus dados abaixo para criar sua conta </p>
             <div className="inputs">
-                <div>
+                <div className='bloco'>
                     <div className='prt-1'>
-                        <p> Nome </p>
-                        <input className='put-1' type='text' />
+                        Nome
+                        <input type='text' />
                     </div>
                     <div className='prt-2'>
-                        <p className='sobre'> Sobrenome</p>
-                        <input className='put-2' />
+                        Sobrenome
+                        <input type='text'/>
                     </div>
                 </div>
                 <div>
                     <div> 
-                        <p className='cpf'> CPF </p>
-                        <input className='put-3' type='text' />
+                        CPF
+                        <input type='text'/>
                     </div>
                     <div>
-                        <p className='cep'> Cep </p>
-                        <input className='put-4' />
+                         Cep
+                        <input type='text'/>
                     </div>
                 </div> 
             </div>
