c8485f76694a7bcff6d5910b4723bd8a4af8696c
igorLAO <igooli670@gmai.com>
2023-09-16 20:51:40 -0300

    API login implementada falta verificar login

--- src/pages/loginPage/login.jsx
+++ src/pages/loginPage/login.jsx
@@ -1,73 +1,100 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import storage from 'local-storage';
 
-import { RecupararSenha } from '../../ui/components/recuperarSenhaComponets/recuperar_senha'
-import './login.scss'
+import { RecupararSenha } from '../../ui/components/recuperarSenhaComponets/recuperar_senha';
+import './login.scss';
 
-// import gameFrame from '../../ui/assets/images/imagesLogin/GAMEBOY_FRAME 1.png'
-import googleLogo from '../../ui/assets/images/imagesLogin/google 1.png'
-import FacebookeLogo from '../../ui/assets/images/imagesLogin/face 1.png'
+import googleLogo from '../../ui/assets/images/imagesLogin/google 1.png';
+import FacebookeLogo from '../../ui/assets/images/imagesLogin/face 1.png';
 
 function Login(props2) {
-  const [hide, setHide] = useState(true)
-  const [reveal, setReveal] = useState(false)
+  const navigate = useNavigate();
+  const [hide, setHide] = useState(true);
+  const [reveal, setReveal] = useState(false);
 
-  const hideReveal = () =>{
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const hideReveal = () => {
     setHide(false)
     setReveal(true)
   }
 
-  const hideReveal2 = () =>{
+  const hideReveal2 = () => {
     setHide(true)
     setReveal(false)
   }
 
-  return (
-    <>
-    <div className='main'>
-    <div className='Frame'>
+  const logar = async () => {
+    try {
+      let res = await axios.post("http://localhost:5000/usuario/login", {
+        Email: email,
+        Senha: senha
+      })
+      if (res.data.Tier == "ADM") 
+          navigate('/ADM');
+      if (res.data.Tier === "NORMAL_USER") 
+            navigate('/perfil-pessoal');
 
-    <div className='DetailsInside'>
-  {hide &&(
-    <>
-    <h2>Ola! seja bem-vindo </h2>
+      storage('usuariologado', res)
       
-        <form action="">
-           <label for='email'>
-            Email
-          <input type="text" id='email'/>
-         </label>
-           <label for='senha'>
-            Senha
-          <input type="password" id='senha'/>
-         </label>
-          <h6 onClick={hideReveal}>Esqueceu a senha</h6>
-          </form>
-        <a href="http://" className='entrarButton'>APERTE PARA ENTRAR</a>
+    } catch (err) {
 
-        <hr className='linha'/>
-     
-      <div className='entrarCom'>
-          <p>Acesse com:</p>
-          <div className='redes'>
-            <img src={FacebookeLogo} alt="" />
-            <img src={googleLogo} alt="" />
-          </div>
-        <h6>Ainda não tem uma conta? <Link to='/cadastro'> <a >Cadastre-se</a></Link> </h6>  
+    if (err.response.status === 401) {
+      console.log(err.response.data.erro)
+    }
+  }
+}
+
+
+return (
+  <>
+    <div className='main'>
+      <div className='Frame'>
+
+        <div className='DetailsInside'>
+          {hide && (
+            <>
+              <h2>Ola! seja bem-vindo </h2>
+
+              <form action="">
+                <label for='email'>
+                  Email
+                  <input type="text" id='email' value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label for='senha'>
+                  Senha
+                  <input type="password" id='senha' value={senha} onChange={e => setSenha(e.target.value)} />
+                </label>
+                <h6 onClick={hideReveal}>Esqueceu a senha</h6>
+              </form>
+              <a onClick={logar} className='entrarButton'> APERTE PARA ENTRAR </a>
+
+              <hr className='linha' />
+
+              <div className='entrarCom'>
+                <p>Acesse com:</p>
+                <div className='redes'>
+                  <img src={FacebookeLogo} alt="" />
+                  <img src={googleLogo} alt="" />
+                </div>
+                <h6>Ainda não tem uma conta? <Link to='/cadastro'> <a >Cadastre-se</a></Link> </h6>
+              </div>
+            </>
+          )}
+        </div>
+
+        {reveal && (
+          <RecupararSenha hideReveal2={hideReveal2} />
+        )}
       </div>
-      </>
-      )}      
-    </div>
-
-    {reveal && (
-      <RecupararSenha hideReveal2={hideReveal2} />
-    )}
-    </div>
 
     </div>
 
-    </>
-  )
+  </>
+)
 }
 
 export default Login
