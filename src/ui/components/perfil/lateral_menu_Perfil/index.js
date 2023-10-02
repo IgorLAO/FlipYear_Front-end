75b49c530f72cb82e767513a338de07f95f8ec47
igorLAO <igooli670@gmail.com>
2023-10-02 08:49:50 -0300

    okok

--- src/ui/components/perfil/lateral_menu_Perfil/index.js
+++ src/ui/components/perfil/lateral_menu_Perfil/index.js
@@ -1,10 +1,29 @@
+import './style.scss'
 
+import CarIcon from '../../../assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
 
 export default function SideBarUsers(){
 
     return(
         <div className="MainSideBar">
-aaaaaaaaaaaaaaaa
+            <div className="op">
+                <span>
+                    <img src='' />
+                    <a> Home </a>
+                </span>
+                <span>
+                    <img src="aa" />
+                    <a> Notificações </a>
+                </span>
+                <span>
+                    <img src={CarIcon} />
+                    <a> Carrinho </a>
+                </span>
+                <span>
+                    <img src="aa" />
+                    <a> Buscar pedidos anteriores </a>
+                </span>
+            </div>
     </div>
     );
 }
