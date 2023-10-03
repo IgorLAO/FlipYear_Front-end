548c15efd0e999cf161dddc32a95e0f051111931
d3nz-17 <denzelnatha30@gmail.com>
2023-10-03 11:56:47 -0300

    carrinho pop up

--- src/pages/carrinho/index.js
+++ src/pages/carrinho/index.js
@@ -3,7 +3,6 @@ import Rodape from '../../ui/components/rodape'
 import CarrinhoIMG from '../../ui/assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
 import Fantasma1 from '../../ui/assets/images/perfil-side-bar/fantasma 2.png'
 import Fantasma2 from '../../ui/assets/images/perfil-side-bar/fantasma4.png'
-
 import './index.scss';
 
 export default function Carrinho(){
@@ -11,6 +10,7 @@ export default function Carrinho(){
     return(
         <div className='pag-carrinho'>
             <NavBar/>
+           
             <div className='pedidos'>
                     <img src={CarrinhoIMG}></img>
                 <div className='titulo'>
