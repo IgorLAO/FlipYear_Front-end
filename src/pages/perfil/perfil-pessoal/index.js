5d39e0afe9bbc5b46db9059e81573195735aa85c
igorLAO <igooli670@gmail.com>
2023-10-06 07:40:57 -0300

    ok

--- src/pages/perfil/perfil-pessoal/index.js
+++ src/pages/perfil/perfil-pessoal/index.js
@@ -1,15 +1,16 @@
 import './index.scss';
 
-import BannerGato from '../../../ui/assets/images/perfil-publico_assets/5759cdb17f90d64d25e9e59854181dde 2.png'
-import Corvo from '../../../ui/assets/images/perfil-publico_assets/download 2.png'
-import CardPedido2 from '../../../ui/components/perfil/card-pedido2'
-import Lapis from '../../../ui/assets/images/perfil-pessoal/105b06c79bc402f 1.png'
-import Carrinho from '../../../ui/assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
+import storage from 'local-storage'
 import { useNavigate } from 'react-router-dom';
 
-import storage from 'local-storage'
+import Corvo from '../../../ui/assets/images/perfil-publico_assets/download 2.png'
+import Lapis from '../../../ui/assets/images/perfil-pessoal/105b06c79bc402f 1.png'
+import Carrinho from '../../../ui/assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
+import filter from '../../../ui/assets/images/adm_assets/filter_icon 1.svg'
+
 import SideBarUsers from '../../../ui/components/perfil/lateral_menu_Perfil';
 import Adm_leftNavBar from '../../../ui/components/ADM_components/left_navbar';
+import CardPedido2 from '../../../ui/components/perfil/card-pedido2'
 
 
 export default function PerfilPessoal() {
@@ -22,40 +23,54 @@ export default function PerfilPessoal() {
 
     return (
         <div className='MainPerfil-P'>
-             <SideBarUsers/> 
-        <div className='perfil-pag'>
-                <div className='banner'>
+            {/* <SideBarUsers/>  */}
+            <div className='perfil-pag'>
+                <div className='banner'></div>
+                <section className='PerfilDetails'>
 
-                </div>
+                    <div className='Perfil'>
+                        <span className='InfoP'>
+                            <img src={Corvo} />
+                            <a> Tyler, the creator </a>
+                        </span>
+                        <span>
 
-            <div className='perfil-infos'>
-                <div className='meio'>
-                    <div className='pfp1'>
-                        <img src={Corvo}></img>
+                            <a className='EditBtn'>Editar Perfil</a>
+                        </span>
                     </div>
-                    <p className='nome'>Ind0m4bl3_Cr0w</p>
-                </div>
 
-                <img className='lapis' src={Lapis}></img>
-                <button onClick={LogOut} style={{width: 75, height: 25}} > LOGOUT </button>
+                    <div className='Pedidos'>
+                            <span className='title'>
+                                <span>
+                                <h1> Meus Pedidos </h1>
+                                </span>
+                                <img src={filter} />
+                            </span>
+                        <div className='Items'>
+                            <CardPedido2 />
+                            <CardPedido2 />
+                            <CardPedido2 />
+                            <CardPedido2 />
+                            <CardPedido2 />
+                        </div>
+
+                        <span className='pages'>
+                            <a>1</a>
+                            <a>2</a>
+                            <a>3</a>
+                            <a>4</a>
+                            <a>5</a>
+                            <a>6</a>
+                            <a>7</a>
+                            <a>8</a>
+                            <a>9</a>
+                            <a>{'>'}</a>
+                        </span>
+                    </div>
+
+                </section>
+
             </div>
-
-
-
-            <div className='pedidos'>
-                <div className='carrinho-titulo'>
-                    <img className='carrinho' src={Carrinho}></img>
-                    <p className='titulo'>Meus Pedidos </p>
-                </div>
-                <div className='linha-branca'> </div>
-            </div>
-
-
-
-        </div>
-
-
-
         </div>
     )
 
--- src/ui/components/perfil/lateral_menu_Perfil/index.js
+++ src/ui/components/perfil/lateral_menu_Perfil/index.js
@@ -1,29 +1,97 @@
-import './style.scss'
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import storage from 'local-storage';
 
-import CarIcon from '../../../assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
+import "./style.scss"
 
-export default function SideBarUsers(){
+import MenuIcon from "../../../assets/images/adm_assets/menuIcon.png"
+import Mail from "../../../assets/images/adm_assets/mail_adm.png"
+import penIcon from "../../../assets/images/adm_assets/lapis_icon.png"
+import searchIcon from "../../../assets/images/adm_assets/search_adm.png"
+import clockIcon from "../../../assets/images/adm_assets/relogio_icon_adm.png"
+import userIcon from "../../../assets/images/adm_assets/usuarioADM_icon.png"
+import statisticsIcon from "../../../assets/images/adm_assets/estatisticasADM_icon.png"
+import logOutIcon from "../../../assets/images/adm_assets/logOut.png"
 
-    return(
-        <div className="MainSideBar">
-            <div className="op">
-                <span>
-                    <img src='' />
-                    <a> Home </a>
-                </span>
-                <span>
-                    <img src="aa" />
-                    <a> Notificações </a>
-                </span>
-                <span>
-                    <img src={CarIcon} />
-                    <a> Carrinho </a>
-                </span>
-                <span>
-                    <img src="aa" />
-                    <a> Buscar pedidos anteriores </a>
+export default function SideBarUsers() {
+    const [splitNav, setSplitNav] = useState();
+    const navigate = useNavigate();
+
+    const logOut = () =>{
+    storage.remove('ADM_Logado')
+    navigate('/login')
+    }
+
+    return (
+        <div style={{ width: 80 }}>
+
+            <div className="MainUsers_bar">
+                <span className="btns">
+
+                    <div className="MenuHeader">
+                        <img src={MenuIcon} />
+                        <h1> Menu </h1>
+                    </div>
+
+
+                    <div className="Options">
+                        <span className="">
+                            <Link to="/">
+                                <div>
+                                    <img src={clockIcon} />
+                                    <h4> Home </h4>
+                                </div>
+                            </Link>
+
+                            <Link>
+                                <div>
+                                    <img src={Mail} />
+                                    <h4> Caixa de Entrada </h4>
+                                </div>
+                            </Link>
+                            <Link to='/ADM_Produtos'>
+                                <div>
+                                    <img src={searchIcon} />
+                                    <h4> Produtos </h4>
+                                </div>
+                            </Link>
+
+                            <Link to='/ADM_RegistroProd'>
+                                <div>
+                                    <img src={penIcon} />
+                                    <h4> Registro </h4>
+                                </div>
+                            </Link>
+
+                            <Link to='/ADM_Users'>
+                                <div>
+                                    <img src={userIcon} />
+                                    <h4> Usuarios </h4>
+                                </div>
+                            </Link>
+
+                            <Link to="ADM_Pedidos">
+                                <div>
+                                    <img src={clockIcon} />
+                                    <h4> Pedidos Pendentes </h4>
+                                </div>
+                            </Link>
+
+
+                            <div>
+                                <img src={statisticsIcon} />
+                                <h4> Estatísticas</h4>
+                            </div>
+                        </span>
+                    </div>
+
+                    <div className="LogOut" onClick={logOut}>
+                        <img src={logOutIcon} />
+                        <h4> Sair</h4>
+                    </div>
+
                 </span>
             </div>
-    </div>
+        </div>
     );
 }
