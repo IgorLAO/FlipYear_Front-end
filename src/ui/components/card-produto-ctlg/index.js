fca3c6448aa0df77760196be8219be72bd8116d2
d3nz15 <denzelnatha30gmail.com>
2023-10-25 11:57:23 -0300

    NÃO SABEMOS O Q TAVA ERRADO PORRA

--- src/ui/components/card-produto-ctlg/index.js
+++ src/ui/components/card-produto-ctlg/index.js
@@ -1,22 +1,19 @@
 import './index.scss';
 import Atari from '../../assets/images/imagesCardProduto/nintendo_Console-removebg-preview 102.png'
-import EstrelaVaziaEsquerda from '../../assets/images/imagesCardProduto/estrelavazia esquerda.png'
-import EstrelaVaziaDireita from '../../assets/images/imagesCardProduto/estrelavaziadireita.png'
-import EstrelaCheiaEsquerda from '../../assets/images/imagesCardProduto/estrelacheiaesquerda.png'
-import EstrelaCheiaDireita from '../../assets/images/imagesCardProduto/estrelacheiadireita.png'
+import EstrelasAvaliacao from './estrelas';
 import Carrrinho from '../../assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
 import { useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// export default function CardProdutoCtlg(props) {
+export default function CardProdutoCtlg(props) {
 
-//     const [qtdProdutos, SetQtdProdutos] = useState(0);
-//     const [limiteQtd, setLimiteQtd] = useState(props.estoque);
-//     const [idUser, setIdUser] = useState(3);
-//     const [idProduto, setIdProduto] = useState(props.idProduto);
-//     const [colecionador, setColecionador] = useState(props.colecionador);
+    const [qtdProdutos, SetQtdProdutos] = useState(0);
+    const [limiteQtd, setLimiteQtd] = useState(props.estoque);
+    const [idUser, setIdUser] = useState(3);
+    const [idProduto, setIdProduto] = useState(props.idProduto);
+    const [colecionador, setColecionador] = useState(props.colecionador);
 
     const [estrelaCheiaEsq1, setEstrelaCheiaEsq1] = useState(false);
     const [estrelaCheiaDir1, setEstrelaCheiaDir1] = useState(false);
@@ -144,276 +141,175 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 
-//     function AddQtdProduto() {
-//         SetQtdProdutos(qtdProdutos + 1);
+    function AddQtdProduto() {
+        SetQtdProdutos(qtdProdutos + 1);
 
-//         if (qtdProdutos >= limiteQtd) {
-//             SetQtdProdutos(limiteQtd);
+        if (qtdProdutos >= limiteQtd) {
+            SetQtdProdutos(limiteQtd);
 
-//         }
-//     }
+        }
+    }
 
-//     function ProdutoAdicionado() {
+    function ProdutoAdicionado() {
 
-//         toast.success("Produto Adicionado ao Carrinho!")
+        toast.success("Produto Adicionado ao Carrinho!")
 
 
-//     }
+    }
 
 
-//     function ErroAdicionarProduto() {
+    function ErroAdicionarProduto() {
 
 
-//         toast.error("Produto Não Adicionado ao Carrinho");
+        toast.error("Produto Não Adicionado ao Carrinho");
 
 
-//     }
+    }
 
-//     function MinusQtdProduto() {
-//         SetQtdProdutos(qtdProdutos - 1);
+    function MinusQtdProduto() {
+        SetQtdProdutos(qtdProdutos - 1);
 
-//         if (qtdProdutos == 0) {
-//             SetQtdProdutos(0);
+        if (qtdProdutos == 0) {
+            SetQtdProdutos(0);
 
-//         }
-//     }
+        }
+    }
 
 
 
 
-//     async function AddNoCarrinho() {
+    async function AddNoCarrinho() {
 
-//         if (qtdProdutos >= 1) {
+        if (qtdProdutos >= 1) {
 
-//             let resposta = await axios.post('http://localhost:5000/carrinho', {
-//                 usuario: idUser,
-//                 produto: idProduto,
-//                 qtd: qtdProdutos
-//             });
-//             let limite = limiteQtd - qtdProdutos;
-//             setLimiteQtd(limiteQtd - qtdProdutos);
+            let resposta = await axios.post('http://localhost:5000/carrinho', {
+                usuario: idUser,
+                produto: idProduto,
+                qtd: qtdProdutos
+            });
+            let limite = limiteQtd - qtdProdutos;
+            setLimiteQtd(limiteQtd - qtdProdutos);
 
-//             if (qtdProdutos > limite) {
-//                 SetQtdProdutos(limite);
+            if (qtdProdutos > limite) {
+                SetQtdProdutos(limite);
 
-//             }
+            }
 
-//             ProdutoAdicionado();
+            ProdutoAdicionado();
 
 
 
-//         }
+        }
 
-//         else {
+        else {
 
 
-//             ErroAdicionarProduto();
+            ErroAdicionarProduto();
 
-//         }
+        }
 
 
 
 
-//     }
+    }
 
-//     return (
-//         <div className='card-produto-ctlg'>
+    return (
+        <div className='card-produto-ctlg'>
 
-//             <ToastContainer></ToastContainer>
-//             {
-//                 (colecionador == true)
-//                     ? <div id='colecionador' className='card'>
+            <ToastContainer></ToastContainer>
+            {
+                (colecionador == true)
+                    ? <div id='colecionador' className='card'>
 
-//                         <div className='qtd-produtos'>
-//                             <img src={Carrrinho} className='card-prod-carrinho' onClick={AddNoCarrinho}></img>
-//                             <p className='hover-opt' onClick={MinusQtdProduto}>-</p>
-//                             <p>{qtdProdutos}</p>
-//                             <p className='hover-opt' onClick={AddQtdProduto}>+</p>
-//                         </div>
+                        <div className='qtd-produtos'>
+                            <img src={Carrrinho} className='card-prod-carrinho' onClick={AddNoCarrinho}></img>
+                            <p className='hover-opt' onClick={MinusQtdProduto}>-</p>
+                            <p>{qtdProdutos}</p>
+                            <p className='hover-opt' onClick={AddQtdProduto}>+</p>
+                        </div>
 
 
-//                         <img src={Atari}></img>
-//                         <p className='nm-produto'> {props.nome} </p>
+                        <img src={Atari}></img>
+                        <p className='nm-produto'> {props.nome} </p>
 
-//                         <div className='desc-info'>
+                        <div className='desc-info'>
 
-//                             <p className='infor-produto' id='cinza-pequeno'> {props.fabricante} </p>
-//                             <div className='ponto'>
-//                                 <p>•</p>
-//                             </div>
-//                             <p className='estado-produto' id='cinza-pequeno'>{props.estado}</p>
-
-//                         </div>
-
-//                         <svg className='linha' xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
-
-//                             <path d="M0.816406 1.22266H282.724" stroke="#0D3A3D" stroke-linecap="round" />
-//                         </svg>
-//                         {
-//                             (props.promocao == 1)
-//                                 ? <>
-//                                     <p className='preco-produto'> ${props.precoPromocao} </p>
-//                                     <p className='preco-produto-original' id='cinza-pequeno'> ${props.preco} </p>
-//                                 </>
-//                                 : <p className='preco-produto'> ${props.preco} </p>
-
-//                         }
-
-//                         <div className='avaliacao-produto'>
-
-                            <div className='estrelas-linha'>
-
-                                <div className='estrelas'>
-                                    {estrelaCheiaEsq1 == true
-
-                                    ? <img src={EstrelaCheiaEsquerda} onMouseLeave={unselectEstrelaEsq} onClick={ClickEsq}></img>
-
-                                    
-                                    :<img src={EstrelaVaziaEsquerda}  
-                                    onMouseEnter={selectEstrelaEsq}
-                                    onClick={ClickEsq}></img>
-
-                                    }
-
-                                    {estrelaCheiaDir1 == true
-   
-                                    ?<img src={EstrelaCheiaDireita} 
-                                    onMouseLeave={() => {unselectEstrelaDir(); unselectEstrelaEsq();}} onClick={ClickDir}></img>
-                                   
-
-                                    :<img src={EstrelaVaziaDireita} 
-                                    onMouseEnter={() => {selectEstrelaDir(); selectEstrelaEsq();}} onClick={ClickDir}></img>
-
-
-                                    }
-
-
-                                </div>
-
-                                <div className='estrelas'>
-                                    {estrelaCheiaEsq2 == true
-
-                                    ? <img src={EstrelaCheiaEsquerda} onMouseLeave={unselectEstEsq2} onClick={ClickEsq}></img>
-
-                                    
-                                    :<img src={EstrelaVaziaEsquerda}  
-                                    onMouseEnter={selectEstEsq2}
-                                    onClick={ClickEsq}></img>
-
-                                    }
-
-                                    {estrelaCheiaDir2 == true
-   
-                                    ?<img src={EstrelaCheiaDireita} 
-                                    onMouseLeave={unselectEstDir2} onClick={ClickDir}></img>
-                                   
-
-                                    :<img src={EstrelaVaziaDireita} 
-                                    onMouseEnter={selectEstDir2} onClick={ClickDir}></img>
-
-
-                                    }
-
-
-                                </div>
-
-  
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
+                            <p className='infor-produto' id='cinza-pequeno'> {props.fabricante} </p>
+                            <div className='ponto'>
+                                <p>•</p>
                             </div>
+                            <p className='estado-produto' id='cinza-pequeno'>{props.estado}</p>
 
-//                             <p className='avaliacao-decimal'>({props.avaliacao})</p>
+                        </div>
 
-//                         </div>
+                        <svg className='linha' xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
 
-//                     </div>
+                            <path d="M0.816406 1.22266H282.724" stroke="#0D3A3D" stroke-linecap="round" />
+                        </svg>
+                        {
+                            (props.promocao == 1)
+                                ? <>
+                                    <p className='preco-produto'> ${props.precoPromocao} </p>
+                                    <p className='preco-produto-original' id='cinza-pequeno'> ${props.preco} </p>
+                                </>
+                                : <p className='preco-produto'> ${props.preco} </p>
 
-//                     : <div className='card'>
-
-//                         <div className='qtd-produtos'>
-//                             <img src={Carrrinho} className='card-prod-carrinho' onClick={AddNoCarrinho}></img>
-//                             <p className='hover-opt' onClick={MinusQtdProduto}>-</p>
-//                             <p>{qtdProdutos}</p>
-//                             <p className='hover-opt' onClick={AddQtdProduto}>+</p>
-//                         </div>
-
-//                         <img src={Atari}></img>
-//                         <p className='nm-produto'> {props.nome} </p>
-
-//                         <div className='desc-info'>
-
-//                             <p className='infor-produto' id='cinza-pequeno'> {props.fabricante} </p>
-//                             <div className='ponto'>
-//                                 <p>•</p>
-//                             </div>
-//                             <p className='estado-produto' id='cinza-pequeno'>{props.estado}</p>
-
-//                         </div>
-
-//                         <svg className='linha' xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
-
-//                             <path d="M0.816406 1.22266H282.724" stroke="#0D3A3D" stroke-linecap="round" />
-//                         </svg>
-//                         {
-//                             (props.promocao == 1)
-
-//                                 ? <>
-//                                     <p className='preco-produto'> ${props.precoPromocao} </p>
-//                                     <p className='preco-produto-original' id='cinza-pequeno'> ${props.preco} </p>
-//                                 </>
-
-//                                 : <p className='preco-produto'> ${props.preco} </p>
-//                         }
+                        }
 
                         <div className='avaliacao-produto'>
-                            <div className='estrelas-linha'>
 
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                            </div>
+                            <EstrelasAvaliacao></EstrelasAvaliacao>
                             <p className='avaliacao-decimal'>({props.avaliacao})</p>
 
-//                         </div>
-//                     </div>
-//             }
-//         </div>
+                        </div>
 
-//     )
-// };
+                    </div>
+
+                    : <div className='card'>
+
+                        <div className='qtd-produtos'>
+                            <img src={Carrrinho} className='card-prod-carrinho' onClick={AddNoCarrinho}></img>
+                            <p className='hover-opt' onClick={MinusQtdProduto}>-</p>
+                            <p>{qtdProdutos}</p>
+                            <p className='hover-opt' onClick={AddQtdProduto}>+</p>
+                        </div>
+
+                        <img src={Atari}></img>
+                        <p className='nm-produto'> {props.nome} </p>
+
+                        <div className='desc-info'>
+
+                            <p className='infor-produto' id='cinza-pequeno'> {props.fabricante} </p>
+                            <div className='ponto'>
+                                <p>•</p>
+                            </div>
+                            <p className='estado-produto' id='cinza-pequeno'>{props.estado}</p>
+
+                        </div>
+
+                        <svg className='linha' xmlns="http://www.w3.org/2000/svg" width="284" height="2" viewBox="0 0 284 2" fill="none">
+
+                            <path d="M0.816406 1.22266H282.724" stroke="#0D3A3D" stroke-linecap="round" />
+                        </svg>
+                        {
+                            (props.promocao == 1)
+
+                                ? <>
+                                    <p className='preco-produto'> ${props.precoPromocao} </p>
+                                    <p className='preco-produto-original' id='cinza-pequeno'> ${props.preco} </p>
+                                </>
+
+                                 : <p className='preco-produto'> ${props.preco} </p>
+                         }
+
+                        <div className='avaliacao-produto'>
+                           <EstrelasAvaliacao></EstrelasAvaliacao>
+                            <p className='avaliacao-decimal'>({props.avaliacao})</p>
+
+                         </div>
+                     </div>
+             }
+         </div>
+
+     )
+ };
