dc55342b6af03dfe43e8c0928ef91377f723143a
d3nz15 <denzelnatha30gmail.com>
2023-10-25 11:50:08 -0300

    estrelas quase la

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 import './index.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
--- src/ui/components/card-produto-ctlg/index.js
+++ src/ui/components/card-produto-ctlg/index.js
@@ -1,9 +1,6 @@
 import './index.scss';
 import Atari from '../../assets/images/imagesCardProduto/nintendo_Console-removebg-preview 102.png'
-import EstrelaVaziaEsquerda from '../../assets/images/imagesCardProduto/estrelavazia esquerda.png'
-import EstrelaVaziaDireita from '../../assets/images/imagesCardProduto/estrelavaziadireita.png'
-import EstrelaCheiaEsquerda from '../../assets/images/imagesCardProduto/estrelacheiaesquerda.png'
-import EstrelaCheiaDireita from '../../assets/images/imagesCardProduto/estrelacheiadireita.png'
+import  EstrelasAvaliacao  from './estrelas';
 import Carrrinho from '../../assets/images/perfil-pessoal/image-removebg-preview (8) 2.png'
 import { useState } from 'react';
 import axios from 'axios';
@@ -18,128 +15,9 @@ export default function CardProdutoCtlg(props) {
     const [idProduto, setIdProduto] = useState(props.idProduto);
     const [colecionador, setColecionador] = useState(props.colecionador);
 
-    const [estrelaCheiaEsq1, setEstrelaCheiaEsq1] = useState(false);
-    const [estrelaCheiaDir1, setEstrelaCheiaDir1] = useState(false);
+    
 
-    const [estrelaCheiaEsq2, setEstrelaCheiaEsq2] = useState(false);
-    const [estrelaCheiaDir2, setEstrelaCheiaDir2] = useState(false);
-
-    const [estrelaCheiaEsq3, setEstrelaCheiaEsq3] = useState(false);
-    const [estrelaCheiaDir3, setEstrelaCheiaDir3] = useState(false);
-
-    const [estrelaCheiaEsq4, setEstrelaCheiaEsq4] = useState(false);
-    const [estrelaCheiaDir4, setEstrelaCheiaDir4] = useState(false);
-
-    const [estrelaCheiaEsq5, setEstrelaCheiaEsq5] = useState(false);
-    const [estrelaCheiaDir5, setEstrelaCheiaDir5] = useState(false);
-
-    const [clicado, setClicado] = useState(false);
-
-    function selectEstrelaEsq(){
-
-        setEstrelaCheiaEsq1(true);
-
-
-
-
-    }
-
-    function selectEstEsq2(){
-
-        setEstrelaCheiaEsq2(true);
-        setEstrelaCheiaDir1(true);
-        setEstrelaCheiaEsq1(true);
-        
-
-
-
-    }
-
-    function selectEstDir2(){
-
-        selectEstEsq2()
-        setEstrelaCheiaDir2(true);
-
-
-
-    }
-
-    function unselectEstEsq2(){
-
-        if(clicado == false){
-
-            setEstrelaCheiaEsq2(false);
-            unselectEstrelaDir()
-            unselectEstrelaEsq()
-
-        }
-
-
-    }
-
-    function unselectEstDir2(){
-
-
-        if(clicado == false){
-
-
-            setEstrelaCheiaDir2(false)
-            unselectEstEsq2();
-
-
-        }
-
-
-    }
-
-    function unselectEstrelaEsq(){
-
-        if(clicado == false){
-
-            setEstrelaCheiaEsq1(false)
-
-        }
-
-    }
-
-    function selectEstrelaDir(){
-
-
-        setEstrelaCheiaDir1(true);
-
-
-    }
-
-    function unselectEstrelaDir(){
-
-        if(clicado == false){
-
-            setEstrelaCheiaDir1(false);
-
-
-        }
-
-
-
-
-    }
-
-    function ClickEsq(){
-
-        setClicado((current) => !current);
-
-
-    }
-
-    function ClickDir(){
-
-        setClicado((current) => !current);
-        selectEstrelaDir();
-        selectEstrelaEsq();
-
-
-    }
-
+    
 
 
 
@@ -261,80 +139,8 @@ export default function CardProdutoCtlg(props) {
 
                         <div className='avaliacao-produto'>
 
-                            <div className='estrelas-linha'>
+                            <EstrelasAvaliacao></EstrelasAvaliacao>
 
-                                <div className='estrelas'>
-                                    {estrelaCheiaEsq1 == true
-
-                                    ? <img src={EstrelaCheiaEsquerda} onMouseLeave={unselectEstrelaEsq} onClick={ClickEsq}></img>
-
-                                    
-                                    :<img src={EstrelaVaziaEsquerda}  
-                                    onMouseEnter={selectEstrelaEsq}
-                                    onClick={ClickEsq}></img>
-
-                                    }
-
-                                    {estrelaCheiaDir1 == true
-   
-                                    ?<img src={EstrelaCheiaDireita} 
-                                    onMouseLeave={() => {unselectEstrelaDir(); unselectEstrelaEsq();}} onClick={ClickDir}></img>
-                                   
-
-                                    :<img src={EstrelaVaziaDireita} 
-                                    onMouseEnter={() => {selectEstrelaDir(); selectEstrelaEsq();}} onClick={ClickDir}></img>
-
-
-                                    }
-
-
-                                </div>
-
-                                <div className='estrelas'>
-                                    {estrelaCheiaEsq2 == true
-
-                                    ? <img src={EstrelaCheiaEsquerda} onMouseLeave={unselectEstEsq2} onClick={ClickEsq}></img>
-
-                                    
-                                    :<img src={EstrelaVaziaEsquerda}  
-                                    onMouseEnter={selectEstEsq2}
-                                    onClick={ClickEsq}></img>
-
-                                    }
-
-                                    {estrelaCheiaDir2 == true
-   
-                                    ?<img src={EstrelaCheiaDireita} 
-                                    onMouseLeave={unselectEstDir2} onClick={ClickDir}></img>
-                                   
-
-                                    :<img src={EstrelaVaziaDireita} 
-                                    onMouseEnter={selectEstDir2} onClick={ClickDir}></img>
-
-
-                                    }
-
-
-                                </div>
-
-  
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                            </div>
 
                             <p className='avaliacao-decimal'>({props.avaliacao})</p>
 
@@ -380,34 +186,9 @@ export default function CardProdutoCtlg(props) {
                         }
 
                         <div className='avaliacao-produto'>
-                            <div className='estrelas-linha'>
 
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                                <div className='estrelas'>
-                                    <img src={EstrelaVaziaEsquerda}></img>
-                                    <img src={EstrelaVaziaDireita}></img>
-                                </div>
-
-                            </div>
+                        <EstrelasAvaliacao></EstrelasAvaliacao>
+                            
                             <p className='avaliacao-decimal'>({props.avaliacao})</p>
 
                         </div>
