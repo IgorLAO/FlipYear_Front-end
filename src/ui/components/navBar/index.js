08c9750b183d652c7d4196ecc50480d881be714b
d3nz15 <denzelnatha30gmail.com>
2023-11-13 07:59:04 -0300

    nossa pesquisa nunca sera vermelha

--- src/ui/components/navBar/index.js
+++ src/ui/components/navBar/index.js
@@ -109,6 +109,7 @@ export default function NavBar() {
             else{
                 setIshideNotFound(false);
                 SetSearchRes([]);
+                document.getElementById('sR').style.display = 'none'
 
             }
           };
