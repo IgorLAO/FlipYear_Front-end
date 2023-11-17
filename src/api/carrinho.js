49a8beb6ea78a9f5e979f9ebf7f2fcff81576ba1
d3nz15 <denzelnatha30gmail.com>
2023-11-17 09:26:56 -0300

    BAGRE

--- src/api/carrinho.js
+++ src/api/carrinho.js
@@ -1,12 +1,12 @@
 import server from "./server";
 
 export async function ConsultaCarrinho(idUser){
-    const resp = await server.get(`/carrinho/${idUser}`);
+    const resp = await server.get(`http://129.148.42.252:5010/carrinho/${idUser}`);
     return resp.data
 }
 
 export async function InserirProdutoNoCarrinho(idProduto, idUser, qtdProduto){
-    const resp = await server.post(`/carrinho/`, {
+    const resp = await server.post(`http://129.148.42.252:5010/carrinho/`, {
         idProduto:idProduto,
         idUser:idUser,
         qtdProduto:qtdProduto
