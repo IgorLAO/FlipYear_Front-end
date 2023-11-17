4632a800efb0023f71b20114251f739f3ede5d08
d3nz15 <denzelnatha30gmail.com>
2023-11-17 09:27:19 -0300

    DESBAGREE

--- src/api/carrinho.js
+++ src/api/carrinho.js
@@ -1,12 +1,12 @@
 import server from "./server";
 
 export async function ConsultaCarrinho(idUser){
-    const resp = await server.get(`http://129.148.42.252:5010/carrinho/${idUser}`);
+    const resp = await server.get(`/carrinho/${idUser}`);
     return resp.data
 }
 
 export async function InserirProdutoNoCarrinho(idProduto, idUser, qtdProduto){
-    const resp = await server.post(`http://129.148.42.252:5010/carrinho/`, {
+    const resp = await server.post(`/carrinho/`, {
         idProduto:idProduto,
         idUser:idUser,
         qtdProduto:qtdProduto
