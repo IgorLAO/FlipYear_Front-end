dad16289d9420dc892c47ef12ab6f2293b0bd47a
Igor_LAO <igoli6767@gmail.com>
2023-11-24 00:41:15 -0300

    MANOOOO

--- src/ui/components/card-produto-ctlg/index.js
+++ src/ui/components/card-produto-ctlg/index.js
@@ -9,26 +9,27 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import localStorage from 'local-storage';
-import { GetUrlImage } from '../../../api/produtos';
+import { ConsultarProdPorId, GetUrlImage } from '../../../api/produtos';
 
 export default function CardProdutoCtlg(props) {
     const navigate = useNavigate();
-    const estoragemLocal = localStorage('ADM_Logado')
+    const estoragemLocal = localStorage('ADM_Logado');
 
     const [colecionador, setColecionador] = useState(props.colecionador);
-    const [fundoBranco, setFundoBranco] = useState('white ')
-    const [fontePreta, setFontePreta] = useState('black ')
-    const [fonteCinza, setFonteCinza] = useState('#252525')
+    const [fundoBranco, setFundoBranco] = useState('white ');
+    const [fontePreta, setFontePreta] = useState('black ');
+    const [fonteCinza, setFonteCinza] = useState('#252525');
+    const [ListIMG, setListIMG] = useState([]);
     const [Lado, setLado] = useState('');
     const [Frente, setFrente] = useState('');
 
-    const [IsMouseEnter, setIsMouseEnter] = useState(false)
+    const [IsMouseEnter, setIsMouseEnter] = useState(false);
 
 
 
     //utilizei o id do produto para seguir para a pág de produto
     function processoCompra() {
-        navigate(`/produto/${props.idProduto}`)
+        navigate(`/produto/${props.idProduto}`);
         window.location.reload();
         window.scrollTo(0, 0);
     }
@@ -51,25 +52,32 @@ export default function CardProdutoCtlg(props) {
 
 
 
-    async function imgFrente() {
-        setFrente(GetUrlImage(props.imgs.ImgFrente))
-        console.log(props.imgs.ImgFrente)
+    async function imgFrente(IMG) {
+        setFrente(GetUrlImage(IMG));
+        console.log(props.imgs.ImgFrente);
     }
 
-    async function imgLado() {
-        setLado(GetUrlImage(props.imgs.ImgLadoEsq))
-        console.log(props.imgs.ImgFrente)
+    async function imgLado(IMG) {
+        setLado(GetUrlImage(IMG));
+        
+        console.log(props.imgs.ImgFrente);
     }
 
+    async function ListImagesById(){
+        let J = Number(props.idProduto)
 
+        let i = await ConsultarProdPorId(J)
 
+        imgFrente(i.FRENTE);
+        imgLado(i.LADO_DIRE);
+        console.log(i, 'alibid')
+        
+    }
 
     useEffect(() => {
         NaoColecionadorCard();
-        imgFrente();
-        imgLado();
-        console.log(props);
-    }, []);
+        ListImagesById()
+    }, [props]);
 
 
 
@@ -81,12 +89,15 @@ export default function CardProdutoCtlg(props) {
             <div
                 className='card'
                 style={{ background: `${fundoBranco}` }}>
-
+          <button onClick={ListImagesById}>
+aaaaaaaaaaaaa
+                </button>
                 <span className='imgsProd'>
                     <img id='ImgProduto' src={Frente} onClick={processoCompra} />
 
                     <img id='Img2Produto' src={Lado} onClick={processoCompra} /> 
                 </span>
+      
 
                 <p className='nm-produto' style={{ color: `${fontePreta}` }}> {props.nome} </p>
 
