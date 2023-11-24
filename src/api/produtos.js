d92df6a4b560d211f28d2f2d1809ef2d715e243a
Igor_LAO <igoli6767@gmail.com>
2023-11-23 23:37:13 -0300

    a

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -84,7 +84,7 @@ export async function InsertProdImages(i) {
 }
 
 export function GetUrlImage(img) {
-    //console.log(`${server.getUri()}/${img}`)
+    console.log(`${server.getUri()}/${img}`)
     return `${server.getUri()}/${img}`
 }
 
